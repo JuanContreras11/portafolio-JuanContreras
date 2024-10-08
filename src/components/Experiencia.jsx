f1efeeae7d477340769ed3c15763a89a46501406
Juan Contreras <84550571+JuanContreras11@users.noreply.github.com>
2024-10-08 15:28:49 -0300

    creacion desplegable

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import experiencia from "../assets/Experiencia.json";
 
 const Experiencia = () => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggleDescription = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
-    <nav className="justify-center text-justify">
+    <nav className="justify-center text-justify py-5">
       <ol className="relative border-s border-gray-200 dark:border-gray-700">
         {experiencia.map((item, index) => (
           <li key={index} className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time className="mb-1 text-sm font-onest  leading-none text-gray-400 dark:text-gray-500">
+            <time className="mb-1 text-sm font-onest leading-none text-gray-400 dark:text-gray-500">
               {item.date}
             </time>
             <h3 className="text-lg font-onest font-bold text-gray-900 dark:text-white">
@@ -17,11 +23,19 @@ const Experiencia = () => {
             <h4 className="text-md font-onest font-semibold text-gray-700 dark:text-gray-300">
               {item.company}
             </h4>
-            <ul className="mb-4 text-base font-onest text-gray-500 dark:text-gray-400">
-              {item.description.split('-').map((point, i) => (
-                <li key={i}>•{point.trim()}</li>
-              ))}
-            </ul>
+            <button
+              onClick={() => toggleDescription(index)}
+              className="text-blue-200 hover:text-blue-600 transition-colors duration-200 " 
+            >
+              {openIndex === index ? "Ocultar descripción" : "Mostrar descripción"}
+            </button>
+            {openIndex === index && (
+              <ul className="mt-2 text-base font-onest text-gray-500 dark:text-gray-400">
+                {item.description.split('-').map((point, i) => (
+                  <li key={i}>• {point.trim()}</li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ol>
