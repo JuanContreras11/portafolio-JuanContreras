a6c808fd8fb55b00f397ab921e91882e0329e687
Juan Contreras <84550571+JuanContreras11@users.noreply.github.com>
2024-10-08 15:28:28 -0300

    creacion componente proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Experiencia from "./components/Experiencia";
 import Section from "./components/Section";
+import Proyectos from "./components/Proyectos";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Section/>
+      <Proyectos />
       <Experiencia />
       {/* agregar iconos de lenguajes y que aparezcan y desaparezcan */}
     </>
